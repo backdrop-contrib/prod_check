575bbeb3aadce512778cb9e8c70d48f481559cda
malc0mn <korneel.k@telenet.be>
2010-12-08 20:47:54 +0000

    Committing to store the project in a safe place. TODO: Fix prod_monitor settings page bug with nagios integration checkbox Nagios testing on our own Nagios setup Add more checks to cover all of drupal-check.org

---
+++ prod_monitor/js/prod-monitor.js
@@ -0,0 +1,9 @@
+/* $Id$ */
+
+/* Prod monitor settings page styling */
+
+$(document).ready(function() {
+  $('#prod-check-settings').equalHeights('px');
+  $('#prod-check-settings').equalWidths('px');
+});
+
