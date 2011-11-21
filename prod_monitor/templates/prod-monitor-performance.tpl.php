fd426314a717d64a6b0baa8f93621e0a4d9b19d0
malc0mn <korneel.k@telenet.be>
2011-11-21 23:54:05 +0100

    Proper output of performance data: one table per unit, grouped per module.

--- prod_monitor/templates/prod-monitor-performance.tpl.php
+++ prod_monitor/templates/prod-monitor-performance.tpl.php
@@ -1,9 +1,39 @@
 <script type='text/javascript' src='http://www.google.com/jsapi'></script>
 
 <?php
+  // TODO: move this first code block to the preprocess function?
+
+  // This data holds all graphs per module, timestamp and per unit (MB, ms, ...).
+  $graphs = array();
   foreach ($data as $module => $data_set) {
-    $graph_title = $cols = $row_data = $row_time = '';
+    // Counters to make sure we only add columns once.
     $count = $i = 0;
+    foreach ($data_set as $time => $params) {
+      // Store title for this modules graphs.
+      $graphs[$module]['title'] = $params['title'];
+      // Count all rows.
+      $count = count($params['data']);
+      foreach ($params['data'] as $title => $row) {
+        if ($i < $count) {
+          // Setup the columns for the graph in such a way that they are
+          // organised by unit so we can draw one graph per unit.
+          $graphs[$module][$row[1]]['cols'][] = "      data.addColumn('number', '$title');";
+          $i++;
+        }
+        // Cast empty strings to 0 and store the data per timestamp and unit so
+        // that we can draw one graph per unit.
+        $graphs[$module][$row[1]]['rows'][$time][] = (int) $row[0];
+      }
+    }
+  }
+
+  // Output all graphs.
+  foreach ($graphs as $module => $data) {
+?>
+<h2><?php print $data['title'] ?></h2>
+<?php
+    unset($data['title']);
+    foreach ($data as $unit => $numbers) {
 ?>
 <script type='text/javascript'>
   google.load('visualization', '1', {'packages':['annotatedtimeline']});
@@ -12,32 +42,27 @@
     var data = new google.visualization.DataTable();
     data.addColumn('datetime', 'Date');
 <?php
-    foreach ($data_set as $time => $params) {
-      $graph_title = $params['title'];
-      $row_time = date('Y, n, j, G, i, s', $time);
-      $count = count($params['data']);
-      $row_tmp = '';
-      foreach ($params['data'] as $title => $row) {
-        if ($i < $count) {
-          $cols .= "    data.addColumn('number', '$title');\n";
-          $i++;
-        }
-        // Cast empty strings to 0.
-        $row_tmp .= (int) $row[0] . ', ';
+      // Add columns.
+      foreach ($numbers['cols'] as $col) {
+        print $col . "\n";
       }
-      $row_data .= "      [new Date($row_time), " . rtrim($row_tmp, ', ') . "],\n";
-    }
-    // TODO: how to handle massive amounts of data here? There's got to be a
-    // beter way. Check the google API.
-    print $cols;
-    print "    data.addRows([\n$row_data    ]);\n";
 ?>
-    var chart = new google.visualization.AnnotatedTimeLine(document.getElementById('<?php print $module ?>'));
+    data.addRows([
+<?php
+      // Add column data.
+      foreach ($numbers['rows'] as $time => $row) {
+        print '      [new Date(' . date('Y, n, j, G, i, s', $time) . '), ' . implode(', ', $row ) . "],\n";
+      }
+?>
+    ]);
+
+    var chart = new google.visualization.AnnotatedTimeLine(document.getElementById('<?php print $module . '-' . $unit; ?>'));
     chart.draw(data, {displayAnnotations: false});
   }
 </script>
-<h2><?php print $graph_title ?></h2>
-<div id="<?php print $module ?>" style="width: 960px; height: 300px;"></div>
+<div id="<?php print $module . '-' . $unit; ?>" style="width: 960px; height: 300px;"></div>
+<p>&nbsp;</p>
 <?php
+    }
   }
 ?>
