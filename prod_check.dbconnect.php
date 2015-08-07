f6d568651755528c2e7867ad17d08fe185d53b71
malc0mn <korneel.k@telenet.be>
2015-08-07 11:13:50 +0200

    Prevent caching of db_connect page!

--- prod_check.dbconnect.php
+++ prod_check.dbconnect.php
@@ -88,4 +88,9 @@ else {
   $msg = 'NOK';
 }
 
+// Prevent caching of this page!
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0"); // Proxies.
+
 exit($msg);
